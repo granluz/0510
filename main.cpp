da0d2b225638b450a19ce8e823e80225aee2c873
granluz <granluz@kakao.com>
2022-05-10 14:15:32 +0900

    0510_1

--- main.cpp
+++ main.cpp
@@ -2,55 +2,61 @@
 
 using namespace std;
 
-//void sum(int , int )
-//{
-//	for(i = 0; i <= *ten; ++i)
-//	
-//		cout << 
-//		return 0;
-//}
+
+
+void SSort(int* Array, int MaxTen)
+{
+	for (int i = 0; i < MaxTen; i++)
+	{
+		for (int j = i; j < MaxTen; j++)
+		{
+			if (Array[i] < Array[j])
+			{
+				int Temp = Array[i];
+				Array[i] = Array[j];
+				Array[j] = Temp;
+			}
+		}
+	}
+}
+int SumNum(int* Array, int MaxTen)
+{
+	int Sum = 0;
+	for (int i = 0; i < MaxTen; i++)
+	{
+		Sum += Array[i];
+	}
+	return Sum;
+}
 
 int main()
 {
 	int MaxTen = 0;
-	
-	int Sum = 0;
 
-			
+
 	cout << "정수의 횟수를 입력하시오. : ";
-	
-	cin >> MaxTen;
-	
-	int* Array = new int [MaxTen];
 
-	cout << "숫자를 입력하시오. :" << endl;
-	
+	cin >> MaxTen;
+
+	int* Array = new int[MaxTen];
+
+	cout << "숫자를 입력하시오." << endl;
+
 	for (int i = 0; i < MaxTen; i++)
 	{
 		cin >> Array[i];
-		Sum += Array[i];
 	}
+	int Sum = SumNum(Array, MaxTen);
+
+
+	cout << "숫자의 합은 : " << Sum << endl;
+
+	cout << "내림차순 정렬" << endl;
+	SSort(Array, MaxTen);
 	for (int i = 0; i < MaxTen; i++)
 	{
-		for (int Step = 0; Step < MaxTen - 1; Step++)
-		{
-
-			for (i = 0; i < MaxTen - 1 - Step; i++)
-			{
-				if (Array[i] < Array[i + 1])
-				{
-					int Temp = Array[i];
-					Array[i] = Array[i + 1];
-					Array[i + 1] = Temp;
-
-				}
-
-			}
-		}
+		cout << Array[i] << endl;
 	}
-	
-	cout << "숫자의 합은 : " << Sum;
 	delete[] Array;
 	return 0;
 }
-
